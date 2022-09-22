aa535f467cce168870821981c3665545383dcbb0
alisiahkoohi <ali.siahkoohi@gmail.com>
2022-09-22 14:29:54 -0500

    json file for q1-2 and q2-4'

--- facvae/utils/upload_results.py
+++ facvae/utils/upload_results.py
@@ -7,15 +7,15 @@ def upload_results(args, flag=""):
     repo_name = "factorialVAE"
     bash_commands = [
         "rclone copy " + flag + " " +
-        checkpointsdir(args.experiment, mkdir=False) + " RiceBox:" +
+        checkpointsdir(args.experiment, mkdir=False) + " MyDropbox:" +
         repo_name +
         checkpointsdir(args.experiment, mkdir=False).replace(gitdir(), ""),
         "rclone copy " + flag + " " +
-        logsdir(args.experiment, mkdir=False) + " RiceBox:" +
+        logsdir(args.experiment, mkdir=False) + " MyDropbox:" +
         repo_name +
         logsdir(args.experiment, mkdir=False).replace(gitdir(), ""),
         "rclone copy " + flag + " " +
-        plotsdir(args.experiment, mkdir=False) + " RiceBox:" +
+        plotsdir(args.experiment, mkdir=False) + " MyDropbox:" +
         repo_name +
         plotsdir(args.experiment, mkdir=False).replace(gitdir(), ""),
     ]
