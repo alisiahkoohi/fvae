260dae6b1d4e623052095b6e0fd7aeda2b3c9e6a
Ali Siahkoohi <alisk@gatech.edu>
2022-11-21 22:21:56 -0600

    add helper bash scripts for submitting jobs

---
+++ scripts/bash_scripts/run_training.sh
@@ -0,0 +1,11 @@
+CUDA_VISIBLE_DEVICES=2 python scripts/train_gmvae.py --h5_filename 3c_2019_window_size-2048_q-6-2_j-6-7_use_day_data-1_avg_pool-0_model_type-scat_filter_key-true.h5 --filter_key '2019' --w_rec 0.05 --max_epoch 1000  --load_to_memory 1 --window_size 2048 --hidden_dim 1024 --nlayer 4 --experiment_name bssa_3c_full-2019-62-scat --ncluster 15 --latent_dim 128 --batchsize 16384 --lr_final 0.001 
+
+python scripts/train_gmvae.py --h5_filename 3c_2019_window_size-2048_q-6-2_j-6-7_use_day_data-1_avg_pool-0_model_type-scat_filter_key-true.h5 --filter_key '2019' --w_rec 0.05 --max_epoch 1000  --load_to_memory 0 --window_size 2048 --hidden_dim 1024 --nlayer 4 --experiment_name bssa_3c_full-2019-62-scat --ncluster 15 --latent_dim 128 --batchsize 16384 --lr_final 0.001 --phase test --cuda 0
+
+CUDA_VISIBLE_DEVICES=2 python scripts/train_gmvae.py --h5_filename 3c_2019_window_size-2048_q-6-2_j-6-7_use_day_data-1_avg_pool-0_model_type-scat_filter_key-true.h5 --filter_key '2019' --w_rec 0.05 --max_epoch 1000  --load_to_memory 1 --window_size 2048 --hidden_dim 1024 --nlayer 4 --experiment_name bssa_3c_full-2019-62-scat --ncluster 20 --latent_dim 128 --batchsize 16384 --lr_final 0.001 
+
+python scripts/train_gmvae.py --h5_filename 3c_2019_window_size-2048_q-6-2_j-6-7_use_day_data-1_avg_pool-0_model_type-scat_filter_key-true.h5 --filter_key '2019' --w_rec 0.05 --max_epoch 1000  --load_to_memory 0 --window_size 2048 --hidden_dim 1024 --nlayer 4 --experiment_name bssa_3c_full-2019-62-scat --ncluster 20 --latent_dim 128 --batchsize 16384 --lr_final 0.001 --phase test --cuda 0
+
+CUDA_VISIBLE_DEVICES=2 python scripts/train_gmvae.py --h5_filename 3c_2019_window_size-2048_q-6-2_j-6-7_use_day_data-1_avg_pool-0_model_type-scat_filter_key-true.h5 --filter_key '2019' --w_rec 0.05 --max_epoch 1000  --load_to_memory 1 --window_size 2048 --hidden_dim 1024 --nlayer 4 --experiment_name bssa_3c_full-2019-62-scat --ncluster 9 --latent_dim 128 --batchsize 16384 --lr_final 0.001
+
+python scripts/train_gmvae.py --h5_filename 3c_2019_window_size-2048_q-6-2_j-6-7_use_day_data-1_avg_pool-0_model_type-scat_filter_key-true.h5 --filter_key '2019' --w_rec 0.05 --max_epoch 1000  --load_to_memory 0 --window_size 2048 --hidden_dim 1024 --nlayer 4 --experiment_name bssa_3c_full-2019-62-scat --ncluster 9 --latent_dim 128 --batchsize 16384 --lr_final 0.001 --phase test --cuda 0 
