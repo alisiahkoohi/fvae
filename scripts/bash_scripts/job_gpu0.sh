57a882345bfdb7396d339a04137328134afe5929
Ali Siahkoohi <ali.siahkoohi@gmail.com>
2023-01-10 13:48:12 -0600

    experiments with nlayer 8

--- scripts/bash_scripts/job_gpu0.sh
+++ scripts/bash_scripts/job_gpu0.sh
@@ -1,13 +1,71 @@
-# CUDA_VISIBLE_DEVICES=0 python scripts/train_gmvae.py --h5_filename 3c_window_size-2048_q-6-6-6_j-7-7-7_use_day_data-1_avg_pool-0_model_type-scat_filter_key-true.h5 --type scat_cov_pca_25 --experiment_name 666-66 --normalize 0 --latent_dim 32 --cuda 1 --ncluster 9 --filter_key '' --nlayer 4
-CUDA_VISIBLE_DEVICES=0 python scripts/train_gmvae.py --h5_filename 3c_window_size-2048_q-6-6-6_j-7-7-7_use_day_data-1_avg_pool-0_model_type-scat_filter_key-true.h5 --type cov_pca_6-6_25 --experiment_name 666-66 --normalize 0 --latent_dim 32 --cuda 1 --ncluster 9 --filter_key '' --nlayer 4
-CUDA_VISIBLE_DEVICES=0 python scripts/train_gmvae.py --h5_filename 3c_window_size-2048_q-6-6-6_j-7-7-7_use_day_data-1_avg_pool-0_model_type-scat_filter_key-true.h5 --type scat_cov_pca_75 --experiment_name 666-66 --normalize 0 --latent_dim 32 --cuda 1 --ncluster 9 --filter_key '' --nlayer 4
-CUDA_VISIBLE_DEVICES=0 python scripts/train_gmvae.py --h5_filename 3c_window_size-2048_q-6-6-6_j-7-7-7_use_day_data-1_avg_pool-0_model_type-scat_filter_key-true.h5 --type scat_cov_pca_25,cov_pca_6-6_25 --experiment_name 666-66_all --normalize 0 --latent_dim 32 --cuda 1 --ncluster 9 --filter_key '' --nlayer 4
-CUDA_VISIBLE_DEVICES=0 python scripts/train_gmvae.py --h5_filename 3c_window_size-2048_q-6-6-6_j-7-7-7_use_day_data-1_avg_pool-0_model_type-scat_filter_key-true.h5 --type scat_cov_pca_25,cov_pca_6-6_75 --experiment_name 666-66_all --normalize 0 --latent_dim 32 --cuda 1 --ncluster 9 --filter_key '' --nlayer 4
-CUDA_VISIBLE_DEVICES=0 python scripts/train_gmvae.py --h5_filename 3c_window_size-2048_q-6-6-6_j-7-7-7_use_day_data-1_avg_pool-0_model_type-scat_filter_key-true.h5 --type scat_cov_pca_25,cov_pca_6-6 --experiment_name 666-66_all --normalize 0 --latent_dim 32 --cuda 1 --ncluster 9 --filter_key '' --nlayer 4
+CUDA_VISIBLE_DEVICES=2 python scripts/train_gmvae.py \
+    --h5_filename 3c_raw_window_size-2048_q-6-2-2_j-7-7-7_use_day_data-1_avg_pool-0_model_type-scat_filter_key-true.h5 \
+    --type scat_cov_pca_25,cov_pca_25 \
+    --experiment_name 622-62_raw \
+    --normalize 0 \
+    --latent_dim 32 \
+    --cuda 1 \
+    --ncluster 9 \
+    --filter_key '' \
+    --nlayer 8 &
 
-CUDA_VISIBLE_DEVICES=0 python scripts/train_gmvae.py --h5_filename 3c_window_size-2048_q-6-6-6_j-7-7-7_use_day_data-1_avg_pool-0_model_type-scat_filter_key-true.h5 --type scat_cov_pca_25 --experiment_name 666-66 --normalize 0 --latent_dim 32 --cuda 1 --ncluster 9 --filter_key '' --nlayer 12
-CUDA_VISIBLE_DEVICES=0 python scripts/train_gmvae.py --h5_filename 3c_window_size-2048_q-6-6-6_j-7-7-7_use_day_data-1_avg_pool-0_model_type-scat_filter_key-true.h5 --type cov_pca_6-6_25 --experiment_name 666-66 --normalize 0 --latent_dim 32 --cuda 1 --ncluster 9 --filter_key '' --nlayer 12
-CUDA_VISIBLE_DEVICES=0 python scripts/train_gmvae.py --h5_filename 3c_window_size-2048_q-6-6-6_j-7-7-7_use_day_data-1_avg_pool-0_model_type-scat_filter_key-true.h5 --type scat_cov_pca_75 --experiment_name 666-66 --normalize 0 --latent_dim 32 --cuda 1 --ncluster 9 --filter_key '' --nlayer 12
-CUDA_VISIBLE_DEVICES=0 python scripts/train_gmvae.py --h5_filename 3c_window_size-2048_q-6-6-6_j-7-7-7_use_day_data-1_avg_pool-0_model_type-scat_filter_key-true.h5 --type scat_cov_pca_25,cov_pca_6-6_25 --experiment_name 666-66_all --normalize 0 --latent_dim 32 --cuda 1 --ncluster 9 --filter_key '' --nlayer 12
-CUDA_VISIBLE_DEVICES=0 python scripts/train_gmvae.py --h5_filename 3c_window_size-2048_q-6-6-6_j-7-7-7_use_day_data-1_avg_pool-0_model_type-scat_filter_key-true.h5 --type scat_cov_pca_25,cov_pca_6-6_75 --experiment_name 666-66_all --normalize 0 --latent_dim 32 --cuda 1 --ncluster 9 --filter_key '' --nlayer 12
-CUDA_VISIBLE_DEVICES=0 python scripts/train_gmvae.py --h5_filename 3c_window_size-2048_q-6-6-6_j-7-7-7_use_day_data-1_avg_pool-0_model_type-scat_filter_key-true.h5 --type scat_cov_pca_25,cov_pca_6-6 --experiment_name 666-66_all --normalize 0 --latent_dim 32 --cuda 1 --ncluster 9 --filter_key '' --nlayer 12
+wait
+
+python scripts/train_gmvae.py \
+    --h5_filename 3c_raw_window_size-2048_q-6-2-2_j-7-7-7_use_day_data-1_avg_pool-0_model_type-scat_filter_key-true.h5 \
+    --type scat_cov_pca_25,cov_pca_25 \
+    --experiment_name 622-62_raw \
+    --normalize 0 \
+    --latent_dim 32 \
+    --cuda 0 \
+    --ncluster 9 \
+    --nlayer 8 \
+    --phase test &
+
+CUDA_VISIBLE_DEVICES=2 python scripts/train_gmvae.py \
+    --h5_filename 3c_raw_window_size-2048_q-6-2-2_j-7-7-7_use_day_data-1_avg_pool-0_model_type-scat_filter_key-true.h5 \
+    --type scat_cov_pca_25 \
+    --experiment_name 622-62_raw \
+    --normalize 0 \
+    --latent_dim 32 \
+    --cuda 1 \
+    --ncluster 9 \
+    --filter_key '' \
+    --nlayer 8 &
+
+wait
+
+python scripts/train_gmvae.py \
+    --h5_filename 3c_raw_window_size-2048_q-6-2-2_j-7-7-7_use_day_data-1_avg_pool-0_model_type-scat_filter_key-true.h5 \
+    --type scat_cov_pca_25 \
+    --experiment_name 622-62_raw \
+    --normalize 0 \
+    --latent_dim 32 \
+    --cuda 0 \
+    --ncluster 9 \
+    --nlayer 8 \
+    --phase test &
+
+CUDA_VISIBLE_DEVICES=2 python scripts/train_gmvae.py \
+    --h5_filename 3c_raw_window_size-2048_q-6-2-2_j-7-7-7_use_day_data-1_avg_pool-0_model_type-scat_filter_key-true.h5 \
+    --type cov_pca_25 \
+    --experiment_name 622-62_raw \
+    --normalize 0 \
+    --latent_dim 32 \
+    --cuda 1 \
+    --ncluster 9 \
+    --filter_key '' \
+    --nlayer 8 &
+
+wait
+
+python scripts/train_gmvae.py \
+    --h5_filename 3c_raw_window_size-2048_q-6-2-2_j-7-7-7_use_day_data-1_avg_pool-0_model_type-scat_filter_key-true.h5 \
+    --type cov_pca_25 \
+    --experiment_name 622-62_raw \
+    --normalize 0 \
+    --latent_dim 32 \
+    --cuda 0 \
+    --ncluster 9 \
+    --nlayer 8 \
+    --phase test
--- scripts/bash_scripts/job_gpu2.sh
+++ scripts/bash_scripts/job_gpu2.sh
@@ -19,7 +19,6 @@ python scripts/train_gmvae.py \
     --latent_dim 32 \
     --cuda 0 \
     --ncluster 9 \
-    --filter_key '' \
     --nlayer 4 \
     --phase test &
 
@@ -44,7 +43,6 @@ python scripts/train_gmvae.py \
     --latent_dim 32 \
     --cuda 0 \
     --ncluster 9 \
-    --filter_key '' \
     --nlayer 4 \
     --phase test &
 
@@ -58,3 +56,16 @@ CUDA_VISIBLE_DEVICES=2 python scripts/train_gmvae.py \
     --ncluster 9 \
     --filter_key '' \
     --nlayer 4 &
+
+wait
+
+python scripts/train_gmvae.py \
+    --h5_filename 3c_raw_window_size-2048_q-6-2-2_j-7-7-7_use_day_data-1_avg_pool-0_model_type-scat_filter_key-true.h5 \
+    --type cov_pca_25 \
+    --experiment_name 622-62_raw \
+    --normalize 0 \
+    --latent_dim 32 \
+    --cuda 0 \
+    --ncluster 9 \
+    --nlayer 4 \
+    --phase test
