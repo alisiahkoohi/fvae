9b7de8cca06288d3ed0279c5a806d01951f76988
Ali Siahkoohi <ali.siahkoohi@gmail.com>
2023-01-11 11:18:32 -0600

    experients with 15 clusters

---
+++ scripts/bash_scripts/job_gpu1.sh
@@ -0,0 +1,71 @@
+CUDA_VISIBLE_DEVICES=1 python scripts/train_gmvae.py \
+    --h5_filename 3c_raw_window_size-2048_q-6-2-2_j-7-7-7_use_day_data-1_avg_pool-0_model_type-scat_filter_key-true.h5 \
+    --type scat_cov_pca_25,cov_pca_25 \
+    --experiment_name 622-62_raw \
+    --normalize 0 \
+    --latent_dim 32 \
+    --cuda 1 \
+    --ncluster 15 \
+    --filter_key '' \
+    --nlayer 8 &
+
+wait
+
+python scripts/train_gmvae.py \
+    --h5_filename 3c_raw_window_size-2048_q-6-2-2_j-7-7-7_use_day_data-1_avg_pool-0_model_type-scat_filter_key-true.h5 \
+    --type scat_cov_pca_25,cov_pca_25 \
+    --experiment_name 622-62_raw \
+    --normalize 0 \
+    --latent_dim 32 \
+    --cuda 0 \
+    --ncluster 15 \
+    --nlayer 8 \
+    --phase test &
+
+CUDA_VISIBLE_DEVICES=1 python scripts/train_gmvae.py \
+    --h5_filename 3c_raw_window_size-2048_q-6-2-2_j-7-7-7_use_day_data-1_avg_pool-0_model_type-scat_filter_key-true.h5 \
+    --type scat_cov_pca_25 \
+    --experiment_name 622-62_raw \
+    --normalize 0 \
+    --latent_dim 32 \
+    --cuda 1 \
+    --ncluster 15 \
+    --filter_key '' \
+    --nlayer 8 &
+
+wait
+
+python scripts/train_gmvae.py \
+    --h5_filename 3c_raw_window_size-2048_q-6-2-2_j-7-7-7_use_day_data-1_avg_pool-0_model_type-scat_filter_key-true.h5 \
+    --type scat_cov_pca_25 \
+    --experiment_name 622-62_raw \
+    --normalize 0 \
+    --latent_dim 32 \
+    --cuda 0 \
+    --ncluster 15 \
+    --nlayer 8 \
+    --phase test &
+
+CUDA_VISIBLE_DEVICES=1 python scripts/train_gmvae.py \
+    --h5_filename 3c_raw_window_size-2048_q-6-2-2_j-7-7-7_use_day_data-1_avg_pool-0_model_type-scat_filter_key-true.h5 \
+    --type cov_pca_25 \
+    --experiment_name 622-62_raw \
+    --normalize 0 \
+    --latent_dim 32 \
+    --cuda 1 \
+    --ncluster 15 \
+    --filter_key '' \
+    --nlayer 8 &
+
+wait
+
+python scripts/train_gmvae.py \
+    --h5_filename 3c_raw_window_size-2048_q-6-2-2_j-7-7-7_use_day_data-1_avg_pool-0_model_type-scat_filter_key-true.h5 \
+    --type cov_pca_25 \
+    --experiment_name 622-62_raw \
+    --normalize 0 \
+    --latent_dim 32 \
+    --cuda 0 \
+    --ncluster 15 \
+    --nlayer 8 \
+    --phase test
