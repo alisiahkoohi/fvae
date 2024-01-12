3444e24c5315ca850252927a70b6940d05adb514
Ali Siahkoohi <ali.siahkoohi@gmail.com>
2024-01-12 17:02:24 -0600

    run al visualizations across the entire mission

--- scripts/reproducing_trained_models/visualize_per_season_full-mission.sh
+++ scripts/reproducing_trained_models/visualize_per_season_full-mission.sh
@@ -13,6 +13,7 @@ CUDA_VISIBLE_DEVICES=2 python $git_root_path/scripts/train_facvae.py \
     --nlayer 4 \
     --filter_key "2019-JUN,2019-JUL,2019-AUG,2019-SEP" \
     --extension full_spring1 \
+    --seed 29 \
     --phase test &
 
 # Summer 1: Sol 306-483
@@ -27,6 +28,7 @@ CUDA_VISIBLE_DEVICES=2 python $git_root_path/scripts/train_facvae.py \
     --nlayer 4 \
     --filter_key "2019-OCT,2019-NOV,2019-DEC,2020-JAN,2020-FEB,2020-MARCH" \
     --extension full_summer1 \
+    --seed 29 \
     --phase test &
 
 wait
@@ -43,6 +45,7 @@ CUDA_VISIBLE_DEVICES=2 python $git_root_path/scripts/train_facvae.py \
     --nlayer 4 \
     --filter_key "2020-APRIL,2020-MAY,2020-JUN,2020-JUL,2020-AUG" \
     --extension full_fall1 \
+    --seed 29 \
     --phase test &
 
 # Winter 1: Sol 625-782
@@ -57,6 +60,7 @@ CUDA_VISIBLE_DEVICES=2 python $git_root_path/scripts/train_facvae.py \
     --nlayer 4 \
     --filter_key "2020-SEP,2020-OCT,2020-NOV,2020-DEC,2021-JAN" \
     --extension full_winter1 \
+    --seed 29 \
     --phase test &
 
 wait
@@ -73,6 +77,7 @@ CUDA_VISIBLE_DEVICES=2 python $git_root_path/scripts/train_facvae.py \
     --nlayer 4 \
     --filter_key "2021-FEB,2021-MARCH,2021-APRIL,2021-MAY,2021-JUN,2021-JUL,2021-AUG" \
     --extension full_spring2 \
+    --seed 29 \
     --phase test &
 
 # Summer 2: Sol 979-1155
@@ -87,6 +92,7 @@ CUDA_VISIBLE_DEVICES=2 python $git_root_path/scripts/train_facvae.py \
     --nlayer 4 \
     --filter_key "2021-SEP,2021-OCT,2021-NOV,2021-DEC,2022-JAN,2022-FEB" \
     --extension full_summer2 \
+    --seed 29 \
     --phase test &
 
 wait
@@ -103,4 +109,5 @@ CUDA_VISIBLE_DEVICES=2 python $git_root_path/scripts/train_facvae.py \
     --nlayer 4 \
     --filter_key "2022-MARCH,2022-APRIL,2022-MAY" \
     --extension full_fall2 \
+    --seed 29 \
     --phase test
